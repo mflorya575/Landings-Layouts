b4b9f544bae0d6c96b764d689b74e9b8c4833f41
Michael Florya <mih2303@mail.ru>
2024-05-29 23:55:51 +0300

    create header

--- 7_sw/js/headerLKBurger.js
+++
@@ -1,6 +0,0 @@
-const burgerHeaderLK = document.querySelector('.header-lk__icon');
-const menuBody = document.querySelector('.header-lk__body');
-
-burgerHeaderLK.addEventListener('click', () => {
-    menuBody.classList.toggle('_open')
-})
